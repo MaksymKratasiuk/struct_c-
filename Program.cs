87ac86ab9610d29fefb68327a494d075e6b36b76
MaksymKratasiuk <makshm1307@gmail.com>
2023-04-18 19:34:42 +0300

    i`ve changed program.css

--- Program.cs
+++ Program.cs
@@ -4,9 +4,33 @@
     {
         static void Main(string[] args)
         {
-            //Task1.task1();
-            //Task2.task2();
-            Task3.task3();
+            Console.WriteLine("Enter task number 1 - 3 0 - exit");
+            while (true)
+            {
+                int number = int.Parse(Console.ReadLine());
+                switch (number)
+                {
+                    case 1:
+                        Task1.task1();
+                        break;
+                    case 2:
+                        Task2.task2();
+                        break;
+                    case 3:
+                        Task3.task3();
+                        break;
+                    case 0:
+                        return;
+                        break;
+                    default:
+                        Console.WriteLine("there`s no such task");
+                        break;
+                }
+
+
+            }
+
+
         }
     }
 }
